716b6ad992cdf0de0d0750e3a560e452e87a8bac
Antonin <antonin63170@gmail.com>
2025-08-26 15:32:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/styles/index.css";
+import { Footer } from "./components";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+createRoot(document.getElementById("footer")).render(
+  <StrictMode>
+    <Footer />
+  </StrictMode>
+);
