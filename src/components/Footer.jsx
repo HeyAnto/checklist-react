f66801982f080eca52c13e77c362cf07bc911cd9
Antonin <antonin63170@gmail.com>
2025-08-26 15:30:34 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        Développé et designé par{" "}
+        <a
+          href="https://github.com/HeyAnto/checklist-react"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          Antonin SIOLY
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
