75f9f7c0e3430f7782fd401e9c240ba1574cf640
Antonin <antonin63170@gmail.com>
2025-08-26 15:32:00 +0200

    Add icons and component exports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as AddTaskButton } from "./AddTaskButton";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as TaskItem } from "./TaskItem";
+export { default as TaskList } from "./TaskList";
